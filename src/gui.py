5976ea207a48f751b7e4d6bcbf3d76cbec719c69
Lucifer481 <novemberking08@gmail.com>
2024-02-12 14:13:02 +0545

    GUI dashboard

--- src/gui.py
+++ src/gui.py
@@ -14,19 +14,21 @@ api_key = os.getenv('VIRUSTOTAL_API_KEY')
 class AntivirusGUI:
     def __init__(self, master):
         self.master = master
-        master.title('Omega Antivirus')
+        master.title('Antivirus')
         master.geometry('1024x768')
 
          # Define colors and load images
-        self.dark_bg = '#333333'
-        self.light_bg = '#F9FEFF'
-        self.accent_color = '#4E9F3D'
+        self.dark_bg = '#0a0000'
+        self.light_bg = '#fafafa'
+        self.accent_color = '#61cf4a'
         self.light_text = "#FFFFFF"
         self.quick_scan_img = ImageTk.PhotoImage(Image.open('img/quick_scan.png'))
         self.system_scan_img = ImageTk.PhotoImage(Image.open('img/full.png'))
         self.custom_scan_img = ImageTk.PhotoImage(Image.open('img/quar.png'))
         self.logo_img = ImageTk.PhotoImage(Image.open('img/logo.png'))
         self.privacy_icon_img = ImageTk.PhotoImage(Image.open('img/logo.png'))
+        self.about_logo_img = ImageTk.PhotoImage(Image.open('img/logo.png'))
+        self.help_icon_img = ImageTk.PhotoImage(Image.open('img/logo.png'))
 
 
         self.setup_sidebar()
@@ -44,5 +46,14 @@ class AntivirusGUI:
             "Setting": lambda: self.update_content("Set your preferences"),
             "Help": lambda: self.update_content("Get Help and Support")
         }
+
+        for text, command in buttons_info.items():
+            button = tk.Button(self.sidebar, text=text, fg=self.light_text, bg=self.dark_bg, bd=0, padx=20, pady=10, command=command)
+            button.pack(fill="x")
+
+    def setup_content_area(self):
+        self.content = tk.Frame(root, bg=self.light_bg)
+        self.content.pack(expand=True, fill='both')
+
 
 
